15f183ecef068e2736dc5af5965e0f20d2286d4c
Rishant Rana <kiturana378@gmail.com>
2023-09-20 18:44:58 +0530

    First Commit Navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../images/Group 77.png"
+
+export default function Hero(){
+    return (
+        <div>
+            <img src={logo}/>
+            <h1>Online Experiences</h1>
+            <h6>Join Unique Interactive led by one-of-a-kind hosts-all without leaving home.</h6>
+        </div>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../images/airbnb1 (1).png"
+
+
+export default function Navbar() {
+    return(
+        <nav className="container">
+            <img src={logo}/>
+        </nav>
+    )
+}
