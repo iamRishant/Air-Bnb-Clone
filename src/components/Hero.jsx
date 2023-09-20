0fd1edab624219d37f90abecc360381c134e64bf
Rishant Rana <kiturana378@gmail.com>
2023-09-20 23:23:49 +0530

    Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import logo from "../images/Group 77.png"
+import logo from "../images/Group 77.png";
 
-export default function Hero(){
-    return (
-        <div>
-            <img src={logo}/>
-            <h1>Online Experiences</h1>
-            <h6>Join Unique Interactive led by one-of-a-kind hosts-all without leaving home.</h6>
-        </div>
-    )
+export default function Hero() {
+  return (
+    <div className="Hero--section">
+      <div className="Hero--img">
+        <img src={logo} />
+      </div>
+
+      <div className="Hero--content">
+        <h1>Online Experiences</h1>
+        <p>
+          Join Unique Interactive led by one-of-a-kind hosts-all without leaving
+          home.
+        </p>
+      </div>
+      
+    </div>
+  );
 }
